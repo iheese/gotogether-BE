9a8f873964a18f7af8134178b99b7e3e7a88d255
iheese <imbc0224@naver.com>
2022-12-01 12:42:41 +0900

    test : 주석 해제

--- src/test/java/com/gotogether/gotogethersbe/GotogetherSBeApplicationTests.java
+++ src/test/java/com/gotogether/gotogethersbe/GotogetherSBeApplicationTests.java
@@ -1,12 +1,11 @@
 package com.gotogether.gotogethersbe;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GotogetherSBeApplicationTests {
-
-//	@Test
-//	void contextLoads() {
-//	}
-////
+public class GotogetherSBeApplicationTests {
+	@Test
+	void contextLoads() {
+	}
 }
